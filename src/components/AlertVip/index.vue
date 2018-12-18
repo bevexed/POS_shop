8af64d155ba27efe7584d1b0546885932d1eee61
bevexed <547551650@qq.com>
2018-12-18 18:30:51 +0800

    会员激活页面 星星 重新确定位置

--- src/components/AlertVip/index.vue
+++ src/components/AlertVip/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="pop" v-show="show">
-    <div style="background:#fff;z-index: 99;padding: 17px;border-radius: 20px;padding-top: 79px">
-      <p>
-        仅限激活会员
-      </p>
-      <p style="margin-bottom: 34px">
-        请点击激活
-      </p>
-      <p style="text-align: center;">
-        <a @click="$emit('hidden',false)">取消</a>
-        <a>激活</a>
-      </p>
+  <div class="back" v-show="show">
+    <div class="pop">
+      <div style="background:#fff;z-index: 99;padding: 17px;border-radius: 20px;padding-top: 79px">
+        <p>
+          仅限激活会员
+        </p>
+        <p style="margin-bottom: 34px">
+          请点击激活
+        </p>
+        <p style="text-align: center;">
+          <a @click="$emit('hidden',false)">取消</a>
+          <a>激活</a>
+        </p>
+      </div>
+      <img src="./alert.svg" alt="">
     </div>
-    <img src="./alert.svg" alt="">
   </div>
 </template>
 
@@ -34,7 +36,16 @@
 </style>
 
 <style scoped lang="less">
-  .pop{
+  .back {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, .3);
+    z-index: 9999;
+  }
+
+  .pop {
     height: 200px;
     width: 90%;
     position: absolute;
@@ -45,11 +56,12 @@
     z-index: 999;
     margin: auto;
   }
+
   img {
     position: absolute;
     width: 232px;
     height: 232px;
-    top: 60%;
+    top: -70%;
     left: 20%;
     z-index: 99999999999999999999999;
   }
@@ -72,6 +84,7 @@
       }
 
       &:last-child {
+        color: white;
         margin-left: 30px;
         background: #ff6d4b;
       }
