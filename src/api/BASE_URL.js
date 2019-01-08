81562cc5221501461e1aebe297331431dce7e31b
bevexed <547551650@qq.com>
2019-01-08 18:34:29 +0800

    修改 api

--- src/api/BASE_URL.js
+++ src/api/BASE_URL.js
@@ -3,13 +3,13 @@
 
 // 上线地址
 // export const BASE_URL = 'http://lzxprogrammer.com'
-export const BASE_URL = 'http://47.105.237.119:8080'
+export const BASE_URL = 'http://47.105.237.119:8081'
 
 // export const IMG_BASE_URL = 'http://lzxprogrammer.com/storage/'
-export const IMG_BASE_URL = 'http://47.105.237.119:8080/storage/'
+export const IMG_BASE_URL = 'http://47.105.237.119:8081/storage/'
 
 
 // export const IMG_URL = 'http://lzxprogrammer.com/storage'
-export const IMG_URL = 'http://47.105.237.119:8080/storage'
+export const IMG_URL = 'http://47.105.237.119:8081/storage'
 
 
