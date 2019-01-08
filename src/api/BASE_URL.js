872e7b6a37dd31c049a12bf3bac4b73dfbb8d0b0
bevexed <547551650@qq.com>
2019-01-08 18:32:47 +0800

    修改端口

--- src/api/BASE_URL.js
+++ src/api/BASE_URL.js
@@ -2,11 +2,14 @@
 // export const BASE_URL = '/api'
 
 // 上线地址
-export const BASE_URL = 'http://lzxprogrammer.com'
+// export const BASE_URL = 'http://lzxprogrammer.com'
+export const BASE_URL = 'http://47.105.237.119:8080'
 
-export const IMG_BASE_URL = 'http://lzxprogrammer.com/storage/'
+// export const IMG_BASE_URL = 'http://lzxprogrammer.com/storage/'
+export const IMG_BASE_URL = 'http://47.105.237.119:8080/storage/'
 
 
-export const IMG_URL = 'http://lzxprogrammer.com/storage'
+// export const IMG_URL = 'http://lzxprogrammer.com/storage'
+export const IMG_URL = 'http://47.105.237.119:8080/storage'
 
 
